3508ef8b0ddc430b31ca3d4abcdd847aa5827b39
Sebastian-IPtoki <81246068+Sebastian-IPtoki@users.noreply.github.com>
2022-07-04 16:45:40 -0400

    Added org.json code

--- java-project/src/main/java/com/yourself/Universe.java
+++ java-project/src/main/java/com/yourself/Universe.java
@@ -2,6 +2,7 @@
 package com.yourself;
 
 import java.util.Arrays;
+import org.json;
 
 public class Universe {
 // }
@@ -11,6 +12,8 @@ public static int countAllStars(int... galaxies) {
 	for(int stars : galaxies) {
 		totalStars = stars; // fix me!
 	} 
+
+	JSONArray jsonSensorsData = new JSONArray();
 
 	System.out.println("Salut Francois");
 	return totalStars;
